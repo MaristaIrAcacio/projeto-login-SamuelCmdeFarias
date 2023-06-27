33e63db598327fc59538454a37c8afc2ef62dba5
Samuel <samucacsg@gmail.com>
2023-06-27 10:05:27 -0300

    adiciona o arquivo controller.js

---
+++ controller.js
@@ -0,0 +1,11 @@
+function Validar(){
+    let email = document.getElementById('loginEmail').value;
+    let senha = document.getElementById('loginSenha').value;
+
+    if(!email || !senha){
+        alert("Campos de preenchimento obrigatório. Favor preencher");
+    }
+    else{
+        alert("Campos preenchidos com sucesso!")
+    }
+}
